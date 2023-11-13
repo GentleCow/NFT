2a0b8227c79a765c0796fce12a0ccc4ce25484c4
absolute <Gio@absolute-Macbook-Prolocal.local>
2023-11-13 14:45:25 +0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import equalibrium from "./style-images/images/equilibrium.jpg";
+import ethereum from "./style-images/images/Icons/icon-ethereum.svg";
+import clock from "./style-images/images/Icons/icon-clock.svg";
+import avatar from "./style-images/images/8aab586e9c05f2d.png";
+import view from "./style-images/images/Icons/icon-view.svg";
+import "./App.css";
+
+import styles from "./style-images/styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <main style={{...styles.size}} >
+   <div className="main">
+    <div className="sizing">
+     <div className="finalHover">
+      <img src={equalibrium} alt="equalibrium" className="imageColor" />
+       <div className="eye"> <img src={view} alt="view" className="vieweye" /> </div>
     </div>
-  );
-}
+    </div> 
+
+   <div style={{...styles.mainContainer}} >
+  <h1 className="hoverMe" style={{...styles.h1}} > Equilibrium #3429 </h1>
+
+     <p style={{...styles.p}}> Our Equilibrium collection promotes balance and calm. </p>
+
+        <div style={{...styles.container}} className="container">
+
+          <div style={{...styles.ethContainer}}> 
+            <div> <img src={ethereum} alt="ethereumIcon" /> </div>
+              0.041 ETH 
+            </div>
+
+          <div style={{...styles.clockContainer}}>
+            <div> <img src={clock} alt="clockIcon" /> </div>
+               3 days left 
+             </div>
+
+        </div> 
+
+        <div style={{...styles.stick}} />
+
+            <div style={{...styles.lastContainer}} >
+             
+             <div> <img src={avatar} alt="avatarIcon"
+             style={{...styles.avatarSize}} /> </div>
+                  <div style={{...styles.text}}>
+                      Contact
+                  </div> 
+
+                  <div> 
+                  <a style={{...styles.textColor}} className="hoverMe2"
+                  href="https://www.facebook.com/giorgi.tayniashvili.108/"
+                  >George Takniashvili</a>
+                      </div>
+
+            </div>
+       </div>
+    </div> 
+  </main> 
+ )
+};
 
 export default App;
---
+++ src/style-images/styles.js
@@ -0,0 +1,54 @@
+const styles = {
+  size: {
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    flexDirection:"column",
+  },
+  mainContainer: {
+    padding:"15px 35px 0px 0px",
+  },
+
+  p: {
+    fontSize: "18px",
+    color:"hsl(215, 51%, 70%)",  // Cyan: hsl(178, 100%, 50%)
+  },
+  container: {
+    marginTop:"30px",
+    display:"flex",
+    gap:"115px",
+    marginBottom:"20px",
+  },
+  stick: {
+    height:"2px",
+    width: "100%",
+    backgroundColor: "hsl(215, 32%, 27%)",
+    marginBottom:"20px",
+  },
+  lastContainer: {
+    display:"flex",
+    alignItems:"center",
+    gap:"4px",
+  },
+  text: {
+    marginLeft: "15px",
+    color: "hsl(215, 51%, 70%)",
+  },
+  ethContainer: {
+    display:"flex",
+    gap:"5px",
+    color:"hsl(178, 100%, 50%)",
+  },
+  clockContainer: {
+    display:"flex",
+    gap:"5px",
+    color:"hsl(215, 51%, 70%)",
+  },
+  avatarSize: {
+    width:"50px",
+    height:"50px",
+    borderRadius:"50%",
+  },
+}
+
+export default styles;
